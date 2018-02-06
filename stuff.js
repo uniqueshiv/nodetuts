b33caf6bcc7be48b38ac03a367cc7a546d1212c0
uniqueshiv <uniqueshivchauhan@gmail.com>
2018-02-06 14:48:58 +0530

    update file

--- stuff.js
+++ stuff.js
@@ -1,6 +1,10 @@
 var adder=function(a,b){
   return `The some of the two number is ${a+b}`;
 }
-pi=3.142;
+var pi=3.142;
+var multiply=function(a,b){
+  return `The multiplication of Two number is ${a*b}`;
+}
 module.exports.adder=adder;
 module.exports.pi=pi;
+module.exports.multiply=multiply;
